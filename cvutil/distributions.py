c69974186525d458f69830a8957ea1b9da066b73
Dan Marchese <marchese.daniel@gmail.com>
2015-10-23 11:02:49 -0400

    Added the Lab color distribution representation shell.

---
+++ cvutil/distributions.py
@@ -0,0 +1,7 @@
+'''Contains utilities for representing the various distributions needed in this project.'''
+
+class LabColorDistribution(object):
+    '''Represents the two-dimensional color distribution using the alpha and beta channels of the
+    Lab color space as axes.
+    '''
+    pass
